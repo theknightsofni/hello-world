943165f8b20bbb9cdd67db38da6547c85a8510f0
theknightsofni <32274297+theknightsofni@users.noreply.github.com>
2021-03-25 22:35:08 -0400

    Add files via upload

---
+++ Hello World.py
@@ -0,0 +1,13 @@
+#hello world
+
+print('Hello World!')
+print('What is your name?')
+myname = input()
+
+print('Hello, ' + myname + ' it is nice to meet you!')
+print('The length of your name is: ')
+print(len(myname))
+
+print('What is your age?')
+myage = input()
+print('Wow, you are old! You will be ' + str(int(myage)+1) + ' next year!')
